2bb3af8d271722fb5425438c082b9678ff0b553d
Richard Lewis <richardlewis@ubuntu.(none)>
2014-04-25 14:30:40 +0100

    css tidy up, added logic to edit a gallery

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,7 +1,7 @@
 class Tournament < ActiveRecord::Base
-	has_many :fixtures
-	has_one :gallery
-	has_many :gallery_images, :through => :gallery
+	has_many :fixtures, dependent: :destroy
+	has_one :gallery, dependent: :destroy
+	has_many :gallery_images, :through => :gallery, dependent: :destroy
 
 	# Validations
     validates :name, presence: { message: 'Add Tournament Name' }
