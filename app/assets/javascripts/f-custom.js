955c946ae9cbdd8f179e3a33ea612bb884e91de0
richard <richard@richard-Inspiron-5523.(none)>
2014-04-26 16:20:58 +0100

    click to reveal fixtures jquery fix

--- app/assets/javascripts/f-custom.js
+++ app/assets/javascripts/f-custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 // Show/Hide Tournaments and Fixtures
    $(".tournament-header").click(function () {
-   	console.log('clicked');
 	      $(this).closest(".table").toggleClass("section-show");
 	    });
 
