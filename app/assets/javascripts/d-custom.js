7c1df5b631e7f9727efdfffdf4e49db719648406
richard <richard@richard-Inspiron-5523.(none)>
2014-04-26 16:16:30 +0100

    alerts added

--- app/assets/javascripts/d-custom.js
+++ app/assets/javascripts/d-custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 // Show/Hide Tournaments and Fixtures
    $(".tournament-header").click(function () {
-   	console.log('clicked');
 	      $(this).closest(".tournament-header").toggleClass("section-show");
 	    });
 
