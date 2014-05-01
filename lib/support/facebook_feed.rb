922b5a357797b1bd4bebcb703fcf4b041fd36ab9
K.Turner <karl.turner5@gmail.com>
2014-05-01 22:39:37 +0100

    Update facebook_feed.rb

--- lib/support/facebook_feed.rb
+++ lib/support/facebook_feed.rb
@@ -1,71 +1,80 @@
 require 'httparty'
 require 'open-uri'
-require 'json'
 
 class FacebookFeed
-  #Constants
-  VANDALS_ID = ENV['VANDALS_FB_ID']
-  FB_ACCESS_TOKEN = ENV['FACEBOOK_ACCESS_TOKEN']
-  CLIENT_ID = ENV['APP_ID']
-  CLIENT_SECRET = ENV['APP_SECRET']
   
-def get_feed
-  fb_access_token = access_token
-  uri = URI(URI.escape "https://graph.facebook.com/#{VANDALS_ID}/posts/?#{fb_access_token}")
-  response = HTTParty.get(uri)
-  results = JSON.parse(response.body)
-  formatted_data(results)  
-end
+  def get_feed
+    results = HTTParty.get(base_url).parsed_response
+    formatted_data(results)  
+  end
 
-def access_token
-  token_uri = URI("https://graph.facebook.com/oauth/access_token?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&grant_type=client_credentials")
-  token_response = HTTParty.get(token_uri)
-end
-
-
-def formatted_data(results)
-    if results['data']
-        for record in results['data'] do
-             attrs = {
-                 message: record['message'],
-                 picture: record['picture'],
-                 link: record['link'],
-                 video_url: record['source'],
-                 object_id: record['object_id'],
-                 description: record['description'],
-                 created_time: record['created_time']
-             }
-
-             Post.where(attrs).first_or_create! do |post|
-              post.attributes = attrs
-             end
-
-             #perform second call
-             if record['object_id']
-                post = Post.find_by object_id: record['object_id']
-                if post
-                    fb_large_picture_url = get_large_photo(record['object_id'])
-                    post.update_column(:large_image_url, fb_large_picture_url)
-                end
-             end
-        end
-        
-        #Delete Posts that have no message (unavailable pages on facebook)
-        delete_if_nil = Post.where(message: nil) 
-        delete_if_nil.destroy_all
-          
+  def formatted_data(results)
+    return unless results['data']
+    results['data'].each do |record|
+      Post.where(record_atts(record)).first_or_create! do |post|
+        post.attributes = record_atts(record)
+      end
+      #perform second call
+      if record['object_id'] && Post.find_by object_id: record['object_id']
+        post = Post.find_by object_id: record['object_id']
+        fb_large_picture_url = get_large_photo(record['object_id'])
+        post.update_column(:large_image_url, fb_large_picture_url)
+      end
     end
- end
+  end
 
- def get_large_photo(object_id)
-     second_uri = URI("https://graph.facebook.com/#{object_id}/?picture")
-     second_response = HTTParty.get(second_uri)
-     second_results = JSON.parse(second_response)
-     formatted_picture_data(second_results)
- end
+   def get_large_photo(object_id)
+    second_uri = URI('https://graph.facebook.com/#{object_id}/?picture')
+    second_results = HTTParty.get(second_uri).parsed_response
+    formatted_picture_data(second_results)
+   end
 
- def formatted_picture_data(second_results)
-  second_results['source']
- end
+  def formatted_picture_data(second_results)
+    second_results['source']
+  end
+
+  private
+
+  def base_url
+    "https://graph.facebook.com/#{vandals_id}/posts/?#{fb_access_token}"
+  end
+
+  def base_token_url
+    "https://graph.facebook.com/oauth/access_token?"
+  end
+
+  def vandals_id
+    ENV['VANDALS_FB_ID']
+  end
+
+  def client_id
+    ENV['APP_ID']
+  end
+
+  def client_secret
+    ENV['APP_SECRET']
+  end
+
+  def fb_access_token
+    required_args = [
+      "client_id=#{client_id}",
+      "client_secret=#{client_secret}",
+      "grant_type=client_credentials"
+    ].join('&')
+    url = token_url + required_args
+    token_response = HTTParty.get(url)
+  end
+
+  def record_atts(record)
+    return unless record
+    {
+      message: record['message'],
+      picture: record['picture'],
+      link: record['link'],
+      object_id: record['object_id'],
+      description: record['description'],
+      created_time: record['created_time']
+    }
+  end
 
 end
